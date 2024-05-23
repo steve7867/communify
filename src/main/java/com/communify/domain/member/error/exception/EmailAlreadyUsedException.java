5ba3c573c14ecd2059a4c4ef58ea5620acef214b
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 22:15:49 +0900

    feat: EmailAlreadyUsedException 생성

---
+++ src/main/java/com/communify/domain/member/error/exception/EmailAlreadyUsedException.java
@@ -0,0 +1,15 @@
+package com.communify.domain.member.error.exception;
+
+import com.communify.global.error.exception.InvalidValueException;
+import lombok.Getter;
+
+@Getter
+public class EmailAlreadyUsedException extends InvalidValueException {
+
+    private final String email;
+
+    public EmailAlreadyUsedException(String email) {
+        super(String.format("%s은 이미 사용 중인 이메일입니다.", email));
+        this.email = email;
+    }
+}
