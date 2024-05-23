220a003316c568cefdb1e033f734cca944bfa9f8
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 17:54:52 +0900

    feat: MemberController 회원 조회 API 추가

--- src/main/java/com/communify/domain/member/presentation/MemberController.java
+++ src/main/java/com/communify/domain/member/presentation/MemberController.java
@@ -1,15 +1,22 @@
-package com.communify.domain.member.controller;
+package com.communify.domain.member.presentation;
 
 import com.communify.domain.member.application.MemberService;
+import com.communify.domain.member.dto.MemberInfo;
 import com.communify.domain.member.dto.MemberSignUpRequest;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/members")
@@ -22,5 +29,11 @@ public class MemberController {
     @ResponseStatus(HttpStatus.CREATED)
     public void signUp(@RequestBody @Valid MemberSignUpRequest request) {
         memberService.signUp(request);
+    }
+
+    @GetMapping("/{memberId}")
+    @ResponseStatus(OK)
+    public MemberInfo getMemberInfo(@PathVariable @NotNull @Positive Long memberId) {
+        return memberService.findMemberInfoById(memberId);
     }
 }
