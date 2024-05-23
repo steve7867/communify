2911a223f42388f8543b76b6cbddbb955d70b912
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 17:39:23 +0900

    feat: login() 메서드 구현

--- src/main/java/com/communify/domain/Auth/application/HttpSessionService.java
+++ src/main/java/com/communify/domain/Auth/application/HttpSessionService.java
@@ -1,7 +1,20 @@
 package com.communify.domain.Auth.application;
 
+import com.communify.domain.member.dto.MemberInfo;
+import com.communify.global.util.SessionKey;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 @Service
 @RequiredArgsConstructor
 public class HttpSessionService implements SessionService{
 
+    private final HttpSession httpSession;
+
+    @Override
+    public void login(MemberInfo memberInfo) {
+        httpSession.setAttribute(SessionKey.MEMBER_ID, memberInfo.getId());
+        httpSession.setAttribute(SessionKey.MEMBER_NAME, memberInfo.getName());
+    }
 }
