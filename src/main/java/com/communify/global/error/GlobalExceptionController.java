a079d82f458070e383eaba5dd5d6f6b46d790732
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-24 03:21:42 +0900

    feat: 파일 업로드 용량 초과 시 예외 처리

--- src/main/java/com/communify/global/error/GlobalExceptionController.java
+++ src/main/java/com/communify/global/error/GlobalExceptionController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.PAYLOAD_TOO_LARGE;
 
 @RestControllerAdvice
 public class GlobalExceptionController {
@@ -32,5 +34,11 @@ public class GlobalExceptionController {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
 
         return new ResponseEntity<>(errorResponse, status);
+    }
+
+    @ResponseStatus(PAYLOAD_TOO_LARGE)
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ErrorResponse handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
+        return new ErrorResponse(e.getLocalizedMessage());
     }
 }
