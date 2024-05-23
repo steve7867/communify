fe20010f42dd3ea5938113ea8c1ff3fda12a191c
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 22:30:53 +0900

    feat: 회원이 존재하지 않을 시 예외 처리

--- src/main/java/com/communify/domain/member/application/MemberService.java
+++ src/main/java/com/communify/domain/member/application/MemberService.java
@@ -6,6 +6,7 @@ import com.communify.domain.member.dto.MemberInfo;
 import com.communify.domain.member.dto.MemberSignUpRequest;
 import com.communify.domain.member.dto.MemberWithdrawRequest;
 import com.communify.domain.member.error.exception.EmailAlreadyUsedException;
+import com.communify.domain.member.error.exception.MemberNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
@@ -27,12 +28,12 @@ public class MemberService {
 
     public MemberInfo findMemberInfoByEmail(String email) {
         return memberRepository.findByEmail(email)
-                .orElseThrow(() -> new RuntimeException()); //todo: 예외 전환 처리
+                .orElseThrow(() -> new MemberNotFoundException(email));
     }
 
     public MemberInfo findMemberInfoById(Long memberId) {
         return memberRepository.findById(memberId)
-                .orElseThrow(() -> new RuntimeException()); //tooo: 예외 처리
+                .orElseThrow(() -> new MemberNotFoundException(memberId));
     }
 
     public void withdraw(MemberWithdrawRequest request, Long memberId) {
