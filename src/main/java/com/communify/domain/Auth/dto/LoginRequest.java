e14ba75a5cd164bc29335c84f93d7b56ddbab70f
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 17:05:10 +0900

    feat: LoginRequest 생성자, getter 추가

--- src/main/java/com/communify/domain/Auth/dto/LoginRequest.java
+++ src/main/java/com/communify/domain/Auth/dto/LoginRequest.java
@@ -1,4 +1,10 @@
 package com.communify.domain.Auth.dto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+@Getter
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 public class LoginRequest {
     private final String email;
     private final String password;
