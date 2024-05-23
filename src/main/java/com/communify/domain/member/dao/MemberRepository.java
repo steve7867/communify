b01be576231a816028f9563d89c914126da23567
steve7867 <parkjunsu3@gmail.com>
2024-05-23 16:20:01 +0900

    feat: MemberRepository 인터페이스 생성

---
+++ src/main/java/com/communify/domain/member/dao/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.communify.domain.member.dao;
+
+import com.communify.domain.member.dto.MemberSignUpRequest;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface MemberRepository {
+
+}
