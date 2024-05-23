6f9ea293ca626c93973d027d46f89ac83bdb4740
steve7867 <parkjunsu3@gmail.com>
2024-05-23 16:17:34 +0900

    feat: MemberService 클래스 생성

---
+++ src/main/java/com/communify/domain/member/application/MemberService.java
@@ -0,0 +1,11 @@
+package com.communify.domain.member.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+
+}
