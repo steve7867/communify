afa8e28a6c5d9c587458b9491b6503b5db64e098
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 17:45:35 +0900

    feat: MemberInfo의 해시값은 Json 포함 제외

--- src/main/java/com/communify/domain/member/dto/MemberInfo.java
+++ src/main/java/com/communify/domain/member/dto/MemberInfo.java
@@ -1,5 +1,6 @@
 package com.communify.domain.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,7 @@ public class MemberInfo {
     private final Long id;
     private final String email;
 
+    @JsonIgnore
     private final String hashed;
     private final String name;
     private final LocalDateTime createdDateTime;
