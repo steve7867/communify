8218f1515c4e3f8414f74a4950b161bd3562850c
steve7867 <parkjunsu3@gmail.com>
2024-05-23 16:09:34 +0900

    feat: MemberSignUpRequest 클래스 필드에 유효성 검증 적용

--- src/main/java/com/communify/domain/member/MemberSignUpRequest.java
+++ src/main/java/com/communify/domain/member/MemberSignUpRequest.java
@@ -1,5 +1,9 @@
 package com.communify.domain.member;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,8 +11,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(force = true)
 public class MemberSignUpRequest {
 
+    @Email(message = "email 형식으로 입력해주세요.")
+    @NotBlank(message = "공백은 허용되지 않습니다.")
+    @Size(max = 50, message = "최대 {0}자까지 허용됩니다.")
     private final String email;
+
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()-_+=<>?/{}|~]{7,20}$",
+            message = "숫자, 영문 대소문자, 공백을 제외한 특수 문자 7 ~ 20자리를 입력해주세요.")
+    @NotBlank(message = "공백은 허용되지 않습니다.")
     private final String password;
+
     private final String hashed;
+
+    @NotBlank(message = "공백은 허용되지 않습니다.")
+    @Size(max = 10, message = "최대 {0}자까지 허용됩니다.")
     private final String name;
 }
