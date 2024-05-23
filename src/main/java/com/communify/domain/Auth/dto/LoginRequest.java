7389c04d2171a5dd64185ddfe8e8727fa5b731a6
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 17:05:40 +0900

    feat: LoginRequest 필드 유효성 검증 로직 추가

--- src/main/java/com/communify/domain/Auth/dto/LoginRequest.java
+++ src/main/java/com/communify/domain/Auth/dto/LoginRequest.java
@@ -1,11 +1,25 @@
 package com.communify.domain.Auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
 @Getter
 @NoArgsConstructor(force = true)
 @RequiredArgsConstructor
 public class LoginRequest {
+
+    @Email(message = "email 형식으로 입력해주세요.")
+    @NotBlank(message = "공백은 허용되지 않습니다.")
+    @Size(max = 50, message = "최대 {0}자까지 허용됩니다.")
     private final String email;
+
+    @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()-_+=<>?/{}|~]{7,20}$",
+            message = "숫자, 영문 대소문자, 공백을 제외한 특수 문자 7 ~ 20자리를 입력해주세요.")
+    @NotBlank(message = "공백은 허용되지 않습니다.")
     private final String password;
 }
