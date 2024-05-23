cc7edb7bf09d043469fbf24901f8fa14826b262f
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 22:35:58 +0900

    feat: InvalidValueException 생성자 추가

--- src/main/java/com/communify/global/error/exception/InvalidValueException.java
+++ src/main/java/com/communify/global/error/exception/InvalidValueException.java
@@ -6,6 +6,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class InvalidValueException extends BusinessException {
 
+    public InvalidValueException(String message) {
+        super(HttpStatus.BAD_REQUEST, message);
+    }
+
     public InvalidValueException(String message, Throwable cause) {
         super(HttpStatus.BAD_REQUEST, message, cause);
     }
