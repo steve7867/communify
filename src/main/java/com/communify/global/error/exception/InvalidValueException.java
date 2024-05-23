1128ff6a41e6b1de5680cc1a1205385a260ebeab
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 22:25:16 +0900

    chore: InvalidAccessException 생성자 수정

--- src/main/java/com/communify/global/error/exception/InvalidValueException.java
+++ src/main/java/com/communify/global/error/exception/InvalidValueException.java
@@ -6,7 +6,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class InvalidValueException extends BusinessException {
 
-    public InvalidValueException(String message) {
-        super(HttpStatus.BAD_REQUEST, message);
+    public InvalidValueException(String message, Throwable cause) {
+        super(HttpStatus.BAD_REQUEST, message, cause);
     }
 }
