4e4dcae81241017036789df7144b3d6731aae8ea
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 17:40:16 +0900

    feat: AuthService 클래스 생성

---
+++ src/main/java/com/communify/domain/Auth/application/AuthService.java
@@ -0,0 +1,30 @@
+package com.communify.domain.Auth.application;
+
+import com.communify.domain.Auth.dto.LoginRequest;
+import com.communify.domain.member.application.MemberService;
+import com.communify.domain.member.dto.MemberInfo;
+import com.communify.global.util.PassEncryptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final MemberService memberService;
+    private final SessionService sessionService;
+
+    public void login(LoginRequest request) {
+        String email = request.getEmail();
+        String password = request.getPassword();
+
+        MemberInfo memberInfo = memberService.findMemberInfoByEmail(email);
+
+        String hashed = memberInfo.getHashed();
+        if (!PassEncryptor.isMatch(password, hashed)) {
+            throw new IllegalArgumentException(); //todo: 예외 처리
+        }
+
+        sessionService.login(memberInfo);
+    }
+}
