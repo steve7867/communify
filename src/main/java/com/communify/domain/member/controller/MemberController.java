d98a1075693ff30a705592f60f3b1766ceb53a55
steve7867 <parkjunsu3@gmail.com>
2024-05-23 16:13:23 +0900

    feat: MemberController 클래스 생성

---
+++ src/main/java/com/communify/domain/member/controller/MemberController.java
@@ -0,0 +1,11 @@
+package com.communify.domain.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberController {
+}
