dadaccfe123fab78c2e768fd200fac4469fb5212
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 17:10:45 +0900

    feat: MemberInfo 생성자, getter 추가

--- src/main/java/com/communify/domain/member/dto/MemberInfo.java
+++ src/main/java/com/communify/domain/member/dto/MemberInfo.java
@@ -1,7 +1,14 @@
 package com.communify.domain.member.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 public class MemberInfo {
 
     private final Long id;
