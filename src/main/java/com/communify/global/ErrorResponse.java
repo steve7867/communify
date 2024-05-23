062721fbec7598441989d17cf16129b1650dfd6b
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 23:43:08 +0900

    feat: ErrorResponse 생성

---
+++ src/main/java/com/communify/global/ErrorResponse.java
@@ -0,0 +1,11 @@
+package com.communify.global;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ErrorResponse {
+
+    private final String message;
+}
