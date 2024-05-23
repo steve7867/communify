fccdd6d5a010ed0606811f36d426ca981973c8df
steve7867 <parkjunsu3@gmail.com>
2024-05-23 16:23:22 +0900

    feat: MemberService 클래스에 signUp 메서드 추가

--- src/main/java/com/communify/domain/member/application/MemberService.java
+++ src/main/java/com/communify/domain/member/application/MemberService.java
@@ -1,6 +1,9 @@
 package com.communify.domain.member.application;
 
+import com.communify.domain.member.dao.MemberRepository;
+import com.communify.domain.member.dto.MemberSignUpRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +12,11 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    public void signUp(MemberSignUpRequest request) {
+        try {
+            memberRepository.insert(request);
+        } catch (DuplicateKeyException e) {
+            throw e; //todo: 예외 전환할 것
+        }
+    }
 }
