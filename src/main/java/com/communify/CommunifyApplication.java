9c436d38a169c671aedb42f9afb27fe807c8947d
steve7867 <parkjunsu3@gmail.com>
2024-05-23 16:02:09 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/communify/CommunifyApplication.java
@@ -0,0 +1,13 @@
+package com.communify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/communify/CommunifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
