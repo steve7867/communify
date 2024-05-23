05bfe9b9529699d4122b3e3508e7302387e4a06c
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-23 17:09:11 +0900

    feat: MemberInfo 클래스 생성

---
+++ src/main/java/com/communify/domain/member/dto/MemberInfo.java
@@ -0,0 +1,11 @@
+package com.communify.domain.member.dto;
+
+public class MemberInfo {
+
+    private final Long id;
+    private final String email;
+
+    private final String hashed;
+    private final String name;
+    private final LocalDateTime createdDateTime;
+}
