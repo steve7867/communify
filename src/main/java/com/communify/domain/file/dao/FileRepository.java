6226c7176547e647bc77d2ada77ecaba478d237d
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-24 20:22:28 +0900

    feat: FileRepository 생성

---
+++ src/main/java/com/communify/domain/file/dao/FileRepository.java
@@ -0,0 +1,10 @@
+package com.communify.domain.file.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface FileRepository {
+
+}
