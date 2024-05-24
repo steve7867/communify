c1bdade013811d2f591e095655e91ffdafbdec31
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-24 16:31:37 +0900

    feat: FollowRepository.findFollowers 메서드 선언

--- src/main/java/com/communify/domain/follow/dao/FollowRepository.java
+++ src/main/java/com/communify/domain/follow/dao/FollowRepository.java
@@ -1,6 +1,9 @@
 package com.communify.domain.follow.dao;
 
+import com.communify.domain.member.dto.MemberInfo;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface FollowRepository {
@@ -8,4 +11,6 @@ public interface FollowRepository {
     void insertFollow(Long memberId, Long followId);
 
     void deleteFollow(Long memberId, Long followId);
+
+    List<MemberInfo> findFollowers(Long memberId);
 }
