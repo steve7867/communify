8f2061909079fb5689c5ef723a4db55a39f22498
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-24 21:30:38 +0900

    feat: FileStorageRequest 생성

--- src/main/java/com/communify/domain/file/application/StorageService.java
+++ src/main/java/com/communify/domain/file/application/StorageService.java
@@ -1,11 +1,11 @@
 package com.communify.domain.file.application;
 
 import com.communify.domain.file.dto.FileInfo;
-import org.springframework.web.multipart.MultipartFile;
+import com.communify.domain.file.dto.FileStorageRequest;
 
 import java.util.List;
 
 public interface StorageService {
 
-    List<FileInfo> saveInFileSystem(List<MultipartFile> multipartFileList, Long postId);
+    List<FileInfo> saveInFileSystem(FileStorageRequest request);
 }
