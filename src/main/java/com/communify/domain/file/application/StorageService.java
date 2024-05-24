59bf6c83a0713f85d6d0963815dc46423dbf90a2
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-24 20:27:21 +0900

    feat: StorageService.saveInFileSystem 선언

--- src/main/java/com/communify/domain/file/application/StorageService.java
+++ src/main/java/com/communify/domain/file/application/StorageService.java
@@ -1,5 +1,11 @@
 package com.communify.domain.file.application;
 
+import com.communify.domain.file.dto.FileInfo;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface StorageService {
 
+    List<FileInfo> saveInFileSystem(List<MultipartFile> multipartFileList, Long postId);
 }
