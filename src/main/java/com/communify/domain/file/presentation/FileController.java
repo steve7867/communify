69179bb0791a2d2b59df3859bc7c65dba8178744
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-25 00:48:24 +0900

    feat: FileController.getFile 추가

--- src/main/java/com/communify/domain/file/presentation/FileController.java
+++ src/main/java/com/communify/domain/file/presentation/FileController.java
@@ -1,6 +1,10 @@
 package com.communify.domain.file.presentation;
 
+import com.communify.domain.file.application.FileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/posts")
 @RequiredArgsConstructor
 public class FileController {
+
+    private final FileService fileService;
+
+    /*
+     *  html 페이지에서 <img src="/posts/image/{storedFilename}"/> 태그가 src의 url로 get 요청을 보낸다.
+     */
+    @GetMapping(value = "/images/{storedFilename}")
+    public Resource getFile(@PathVariable String storedFilename) {
+        return fileService.getFileInfoAndResource(storedFilename).getResource();
+    }
 }
