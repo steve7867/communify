dc3ddf01614b89662408bbb9343cf407bd24e110
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-24 19:55:19 +0900

    feat: CommentService.getComments 추가

--- src/main/java/com/communify/domain/comment/application/CommentService.java
+++ src/main/java/com/communify/domain/comment/application/CommentService.java
@@ -1,9 +1,14 @@
 package com.communify.domain.comment.application;
 
 import com.communify.domain.comment.dao.CommentRepository;
+import com.communify.domain.comment.dto.CommentInfo;
 import com.communify.domain.comment.dto.CommentUploadRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +19,11 @@ public class CommentService {
     public void addComment(CommentUploadRequest request) {
         commentRepository.insert(request);
         //todo: 이벤트 처리
+    }
+
+    @Transactional(readOnly = true) //todo: 캐싱 적용
+    public List<CommentInfo> getComments(Long postId) {
+        List<CommentInfo> commentInfoList = commentRepository.findAllCommentsByPostId(postId);
+        return Collections.unmodifiableList(commentInfoList);
     }
 }
