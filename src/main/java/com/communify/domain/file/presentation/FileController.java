c1f5e628f07e3c5988687f43a6910cd94142e74f
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-25 00:47:02 +0900

    feat: FileController 생성

---
+++ src/main/java/com/communify/domain/file/presentation/FileController.java
@@ -0,0 +1,11 @@
+package com.communify.domain.file.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+public class FileController {
+}
