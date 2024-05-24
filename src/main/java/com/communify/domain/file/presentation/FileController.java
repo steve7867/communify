290656d0337689ebe6fa84c380bdfacbb7147b7d
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-25 00:57:56 +0900

    feat: FileController.downloadFile 추가

--- src/main/java/com/communify/domain/file/presentation/FileController.java
+++ src/main/java/com/communify/domain/file/presentation/FileController.java
@@ -1,12 +1,19 @@
 package com.communify.domain.file.presentation;
 
 import com.communify.domain.file.application.FileService;
+import com.communify.domain.file.dto.FileInfo;
+import com.communify.domain.file.dto.FileInfoAndResource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriUtils;
+
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/posts")
@@ -21,5 +28,24 @@ public class FileController {
     @GetMapping(value = "/images/{storedFilename}")
     public Resource getFile(@PathVariable String storedFilename) {
         return fileService.getFileInfoAndResource(storedFilename).getResource();
+    }
+
+    /*
+        HTTP Response Header에서 Content-Disposition 헤더 값을 attachment; filename="image.jpg"와 같이 설정하면
+        브라우저는 image.jpg라는 파일을 다운로드한다.
+     */
+    @GetMapping(value = "/images/{storedFilename}/download")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String storedFilename) {
+        FileInfoAndResource fileInfoAndResource = fileService.getFileInfoAndResource(storedFilename);
+
+        Resource resource = fileInfoAndResource.getResource();
+        FileInfo fileInfo = fileInfoAndResource.getFileInfo();
+
+        String originalFilename = fileInfo.getOriginalFilename();
+        String encodedOriginalFilename = UriUtils.encode(originalFilename, StandardCharsets.UTF_8);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + encodedOriginalFilename + "\"")
+                .body(resource);
     }
 }
