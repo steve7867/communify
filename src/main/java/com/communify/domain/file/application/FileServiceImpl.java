fc09dd7d2ce848d32d49ece3dac07357e228f4f3
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-24 21:43:55 +0900

    feat: FileServiceImpl.uploadFile 구현

--- src/main/java/com/communify/domain/file/application/FileServiceImpl.java
+++ src/main/java/com/communify/domain/file/application/FileServiceImpl.java
@@ -1,8 +1,31 @@
 package com.communify.domain.file.application;
 
+import com.communify.domain.file.dao.FileRepository;
+import com.communify.domain.file.dto.FileInfo;
+import com.communify.domain.file.dto.FileStorageRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
+@RequiredArgsConstructor
 public class FileServiceImpl implements FileService {
 
+    private final FileRepository fileRepository;
+    private final StorageService storageService;
+
+    @Override
+    public void uploadFile(List<MultipartFile> multipartFileList, Long postId) {
+        if (Objects.isNull(multipartFileList) || multipartFileList.isEmpty()) {
+            return;
+        }
+
+        FileStorageRequest fileStorageRequest = new FileStorageRequest(postId, multipartFileList);
+        List<FileInfo> fileInfoList = storageService.saveInFileSystem(fileStorageRequest);
+
+        fileRepository.insertFileInfoList(fileInfoList);
+    }
 }
