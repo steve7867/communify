ddb6442793c9be73098b505c16a6827c25199fef
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-24 15:16:01 +0900

    feat: FollowController 생성

---
+++ src/main/java/com/communify/domain/follow/presentation/FollowController.java
@@ -0,0 +1,11 @@
+package com.communify.domain.follow.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class FollowController {
+}
