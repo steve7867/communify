3fb671e93947573bedd812e0acdcab285c91f599
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-24 19:44:59 +0900

    feat: CommentController 생성

---
+++ src/main/java/com/communify/domain/comment/presentation/CommentController.java
@@ -0,0 +1,10 @@
+package com.communify.domain.comment.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class CommentController {
+
+}
