ee479154eb4a9bfdc623ccdb189748c5ed3a88ec
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-26 16:16:21 +0900

    feat: 카테고리 목록 캐싱 적용

--- src/main/java/com/communify/domain/catetory/application/CategoryService.java
+++ src/main/java/com/communify/domain/catetory/application/CategoryService.java
@@ -2,7 +2,9 @@ package com.communify.domain.catetory.application;
 
 import com.communify.domain.catetory.dao.CategoryRepository;
 import com.communify.domain.catetory.dto.CategoryInfo;
+import com.communify.global.util.CacheNames;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +18,7 @@ public class CategoryService {
     private final CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
+    @Cacheable(CacheNames.CATEGORIES)
     public List<CategoryInfo> getAllCategories() {
         List<CategoryInfo> categoryInfoList = categoryRepository.findAll();
         return Collections.unmodifiableList(categoryInfoList);
