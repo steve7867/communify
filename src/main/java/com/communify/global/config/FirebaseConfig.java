850beb686542fc6e614a179ba9024daaf9a625cf
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-26 21:08:30 +0900

    feat: FirebaseConfig 생성

---
+++ src/main/java/com/communify/global/config/FirebaseConfig.java
@@ -0,0 +1,33 @@
+package com.communify.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${firebase.account.key.path}")
+    private String keyPath;
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging(ResourceLoader resourceLoader) throws IOException {
+        Resource resource = resourceLoader.getResource(keyPath);
+        InputStream serviceAccount = resource.getInputStream();
+
+        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseMessaging.getInstance(FirebaseApp.initializeApp(firebaseOptions));
+    }
+}
