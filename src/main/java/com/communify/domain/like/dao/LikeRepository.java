e840cdcc460d0991d1e1e2579c69b236f24fb474
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-26 14:40:17 +0900

    fix: LikeRepository에 @Mapper 어노테이션 추가

--- src/main/java/com/communify/domain/like/dao/LikeRepository.java
+++ src/main/java/com/communify/domain/like/dao/LikeRepository.java
@@ -2,10 +2,14 @@ package com.communify.domain.like.dao;
 
 import com.communify.domain.like.dto.LikeRequest;
 import com.communify.global.dao.BulkInsertable;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 
+@Mapper
+@Repository
 public interface LikeRepository extends BulkInsertable<LikeRequest> {
 
     void insert(@Param("request") LikeRequest request);
