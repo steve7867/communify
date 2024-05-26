1c4bbcb69f4c714e14b343d39baa080267971d1b
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-26 16:21:29 +0900

    feat: CacheNames.POST_OUTLINES 추가

--- src/main/java/com/communify/global/util/CacheNames.java
+++ src/main/java/com/communify/global/util/CacheNames.java
@@ -3,6 +3,7 @@ package com.communify.global.util;
 public final class CacheNames {
 
     public static final String CATEGORIES = "Categories";
+    public static final String POST_OUTLINES = "PostOutlines";
 
     public static final String POST_VIEW = "PostView";
     public static final String POST_LIKE = "PostLike";
