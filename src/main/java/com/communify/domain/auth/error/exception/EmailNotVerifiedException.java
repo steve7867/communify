0f7f9c52e36bdc0740747df436f1a2b79c98e9b5
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-30 03:08:23 +0900

    feat: EmailNotVerifiedException 예외 추가

---
+++ src/main/java/com/communify/domain/auth/error/exception/EmailNotVerifiedException.java
@@ -0,0 +1,16 @@
+package com.communify.domain.auth.error.exception;
+
+import com.communify.global.error.exception.InvalidAccessException;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class EmailNotVerifiedException extends InvalidAccessException {
+
+    private final String email;
+
+    public EmailNotVerifiedException(String email) {
+        super(HttpStatus.BAD_REQUEST, String.format("%s은 인증되지 않은 이메일입니다. 먼저 인증을 진행해주세요.", email));
+        this.email = email;
+    }
+}
