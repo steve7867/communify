b963ce407ae57f8b23395728ba48161db3a5303e
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-30 03:01:59 +0900

    feat: MemberController.requestEmailVerification 추가

--- src/main/java/com/communify/domain/member/presentation/MemberController.java
+++ src/main/java/com/communify/domain/member/presentation/MemberController.java
@@ -3,6 +3,7 @@ package com.communify.domain.member.presentation;
 import com.communify.domain.auth.annotation.CurrentMemberId;
 import com.communify.domain.auth.annotation.LoginCheck;
 import com.communify.domain.auth.annotation.NotLoginCheck;
+import com.communify.domain.auth.application.AuthService;
 import com.communify.domain.member.application.MemberFindService;
 import com.communify.domain.member.application.MemberSignUpService;
 import com.communify.domain.member.application.MemberUpdateService;
@@ -11,6 +12,7 @@ import com.communify.domain.member.dto.MemberInfo;
 import com.communify.domain.member.dto.MemberSignUpRequest;
 import com.communify.domain.member.dto.MemberWithdrawRequest;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -25,6 +27,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.OK;
@@ -38,6 +42,24 @@ public class MemberController {
     private final MemberFindService memberFindService;
     private final MemberUpdateService memberUpdateService;
     private final MemberWithdrawService memberWithdrawService;
+    private final AuthService authService;
+
+    @PostMapping("/email/verification-request")
+    @NotLoginCheck
+    public ResponseEntity<Object> requestEmailVerification(@RequestBody @Email @NotBlank String email) {
+        Optional<MemberInfo> memberInfoOpt = memberFindService.findMemberInfoByEmail(email);
+
+        if (memberInfoOpt.isPresent()) {
+            Map<String, String> map = new HashMap<>();
+            map.put("message", "이미 사용 중인 email입니다.");
+
+            return ResponseEntity.badRequest().body(map);
+        }
+
+        authService.publishEmailVerificationCode(email);
+
+        return ResponseEntity.ok().build();
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
