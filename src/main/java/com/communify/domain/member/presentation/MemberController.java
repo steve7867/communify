6ac570b1c24b59828ebcf61af5aae67536a4a593
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-05-30 14:10:57 +0900

    fix: 누락된 import문 추가

--- src/main/java/com/communify/domain/member/presentation/MemberController.java
+++ src/main/java/com/communify/domain/member/presentation/MemberController.java
@@ -27,6 +27,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 
