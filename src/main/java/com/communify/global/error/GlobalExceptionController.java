cff0a74f264b2d6260dce3df166c07db4c228ce7
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-06-02 14:39:09 +0900

    fix: 메서드 인자 유효성 검증 실패 시 노출되는 예외 메시지 수정

--- src/main/java/com/communify/global/error/GlobalExceptionController.java
+++ src/main/java/com/communify/global/error/GlobalExceptionController.java
@@ -4,12 +4,17 @@ import com.communify.global.error.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.PAYLOAD_TOO_LARGE;
@@ -21,12 +26,21 @@ public class GlobalExceptionController {
     /*
     메서드 인자 유효성 검증 실패 시에 발생하는 MethodArgumentNotValidException를 처리.
      */
-    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.error(e.getMessage(), e);
 
-        return new ErrorResponse(e.getMessage());
+        BindingResult bindingResult = e.getBindingResult();
+        List<ObjectError> errorList = bindingResult.getAllErrors();
+        String message = errorList.get(0).getDefaultMessage();
+
+        ErrorResponse errorResponse = new ErrorResponse(message);
+
+        ProblemDetail body = e.getBody();
+        int status = body.getStatus();
+        HttpStatus httpStatus = HttpStatus.resolve(status);
+
+        return new ResponseEntity<>(errorResponse, httpStatus);
     }
 
     /*
