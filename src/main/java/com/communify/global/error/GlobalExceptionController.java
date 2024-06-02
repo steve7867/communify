0dd75d7a7c7e8340ee111b08520b742e96ec0849
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-06-02 23:14:51 +0900

    feat: RFC 7807 형식에 맞게 Json 응답

--- src/main/java/com/communify/global/error/GlobalExceptionController.java
+++ src/main/java/com/communify/global/error/GlobalExceptionController.java
@@ -3,70 +3,61 @@ package com.communify.global.error;
 import com.communify.global.error.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.multipart.MaxUploadSizeExceededException;
-
-import java.util.List;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.PAYLOAD_TOO_LARGE;
 
+/*
+ * ResponseEntityExceptionHandler는 Http Request 처리 과정에서
+ * Spring MVC가 발생시키는 많은 예외를 처리하여 적절한 예외 메시지를 반환해준다.
+ * RFC 7807(RFC 9457) 형식에 따라 예외 메시지를 작성한다.
+ */
 @RestControllerAdvice
 @Slf4j
-public class GlobalExceptionController {
+public class GlobalExceptionController extends ResponseEntityExceptionHandler {
 
     /*
      * @Valid 어노테이션이 붙은 메서드 인자의 유효성 검증 실패 시에 발생하는 MethodArgumentNotValidException를 처리.
+     * MethodArgumentNotValidException의 기본 메시지만으로는 클라이언트가 문제를 파악할 수 없으므로 커스터마이징 해준다.
      */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        log.error(e.getMessage(), e);
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        String message = ex.getBindingResult()
+                .getAllErrors()
+                .get(0)
+                .getDefaultMessage();
 
-        BindingResult bindingResult = e.getBindingResult();
-        List<ObjectError> errorList = bindingResult.getAllErrors();
-        String message = errorList.get(0).getDefaultMessage();
-
-        ErrorResponse errorResponse = new ErrorResponse(message);
-
-        ProblemDetail body = e.getBody();
-        int status = body.getStatus();
-        HttpStatus httpStatus = HttpStatus.resolve(status);
-
-        return new ResponseEntity<>(errorResponse, httpStatus);
+        return handleExceptionInternal(ex, ProblemDetail.forStatusAndDetail(status, message), headers, status, request);
     }
 
     /*
      * BusinessException과 그 하위 예외를 모두 처리.
      */
     @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+    public ProblemDetail handleBusinessException(BusinessException e) {
         log.error(e.getMessage(), e);
 
-        HttpStatus status = e.getStatus();
-        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
-
-        return new ResponseEntity<>(errorResponse, status);
+        return ProblemDetail.forStatusAndDetail(e.getStatus(), e.getMessage());
     }
 
-    @ResponseStatus(PAYLOAD_TOO_LARGE)
-    @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ErrorResponse handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e) {
-        log.error(e.getMessage(), e);
-
-        return new ErrorResponse(e.getMessage());
-    }
-
-    @ResponseStatus(BAD_REQUEST)
+    /*
+     * 데이터 무결성 제약을 깨는 데이터 삽입, 수정이 발생할 때 던져지는 DataIntegrityViolationException을 처리
+     */
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public void handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+    public ProblemDetail handleDataIntegrityViolationException(DataIntegrityViolationException e) {
         log.error(e.getMessage(), e);
+
+        return ProblemDetail.forStatusAndDetail(BAD_REQUEST, e.getMessage());
     }
 }
