e29748265024585d5dfba342125ede4e1e39866b
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-06-03 23:57:15 +0900

    fix: Validator 적용 대상 객체 수정

--- src/main/java/com/communify/domain/post/presentation/PostController.java
+++ src/main/java/com/communify/domain/post/presentation/PostController.java
@@ -62,12 +62,12 @@ public class PostController {
     /*
      * @InitBinder는 해당 컨트롤러로 들어오는 모든 요청에 대해 추가적인 설정을 할 때 사용한다.
      * 모든 요청에 대해 본래 메서드 호출 전에 @InitBinder 메서드를 먼저 호출한다.
-     * 특정 객체에만 적용하고자 할 때는 @InitBinder("PostUploadRequest")와 같이 사용한다.
-     * 그럼, PostUploadRequest 객체에 대해서만 @InitBinder를 호출한다.
+     * 특정 객체에만 적용하고자 할 때는 @InitBinder("postUploadForm")와 같이 사용한다.
+     * 그럼, PostUploadForm 객체에 대해서만 @InitBinder를 호출한다.
      * WebDataBinder에 검증기를 추가하면 validation을 자동으로 적용할 수 있다.
      */
-    @InitBinder("PostUploadRequest")
-    public void addPostUploadRequestValidator(WebDataBinder dataBinder) {
+    @InitBinder("postUploadForm")
+    public void addPostUploadFormValidator(WebDataBinder dataBinder) {
         dataBinder.addValidators(postUploadFormValidator);
     }
 
