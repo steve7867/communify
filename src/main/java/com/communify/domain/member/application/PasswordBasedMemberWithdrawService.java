7b3cd96a50023b25c2bd648cebec90252f7438da
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-06-04 04:05:02 +0900

    refactor: MemberWithdrawService.withdraw 인자 변경

--- src/main/java/com/communify/domain/member/application/PasswordBasedMemberWithdrawService.java
+++ src/main/java/com/communify/domain/member/application/PasswordBasedMemberWithdrawService.java
@@ -19,7 +19,9 @@ public class PasswordBasedMemberWithdrawService implements MemberWithdrawService
     private final LoginService loginService;
 
     @Override
-    public void withdraw(MemberWithdrawRequest request, Long memberId) {
+    public void withdraw(MemberWithdrawRequest request) {
+        Long memberId = request.getMemberId();
+
         MemberInfo memberInfo = memberFindService.findMemberInfoById(memberId)
                 .orElseThrow(() -> new MemberNotFoundException(memberId));
 
