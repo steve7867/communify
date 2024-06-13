c74f59ea9e8830d65f069e7e476878a74a052ee3
steve7867 <115217247+steve7867@users.noreply.github.com>
2024-06-13 15:21:05 +0900

    chore: MemberSignUpRequest 생성자 수정, 파일 업로드 용량 수정

--- src/main/java/com/communify/domain/member/dto/incoming/MemberSignUpRequest.java
+++ src/main/java/com/communify/domain/member/dto/incoming/MemberSignUpRequest.java
@@ -2,15 +2,14 @@ package com.communify.domain.member.dto.incoming;
 
 import com.communify.global.util.PasswordEncryptor;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(force = true)
 public class MemberSignUpRequest {
 
     @Email
@@ -19,18 +18,21 @@ public class MemberSignUpRequest {
     private final String email;
 
     @NotBlank
+    @Size(min = 1, max = 10)
+    private final String name;
+
+    @NotBlank
     @Pattern(regexp = "^[a-zA-Z0-9!@#$%^&*()-_+=<>?/{}|~]{7,20}$",
             message = "숫자, 영문 대소문자, 공백을 제외한 특수 문자 7 ~ 20자리를 입력해주세요.")
     private final String password;
 
     private final String hashed;
 
-    @NotBlank
-    @Size(min = 1, max = 10)
-    private final String name;
-
     @JsonCreator
-    public MemberSignUpRequest(String email, String password, String name) {
+    public MemberSignUpRequest(@JsonProperty("email") String email,
+                               @JsonProperty("password") String password,
+                               @JsonProperty("name") String name) {
+
         this.email = email;
         this.password = password;
         this.name = name;
